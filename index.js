01d72bb770f93b847fd82f4d678d7b82c6247e0b
Bogdan Stashchuk <43352618+bstashchuk@users.noreply.github.com>
2024-04-17 12:28:16 +0100

    Modify server response for the root path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.send('Hello World from the Express Web Server');
 });
 
 app.listen(3000);
